18a488723a18bcef64cf5ae726a07d284d4902c8
lerghin <lerghincardenas@gmail.com>
2022-12-22 15:25:56 -0400

    2212

--- src/main/java/com/portfoliolerghin/lc/service/SExperiencia.java
+++ src/main/java/com/portfoliolerghin/lc/service/SExperiencia.java
@@ -12,35 +12,34 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class SExperiencia {
-    @Autowired
-    RExperiencia rExperiencia;
-    public List<Experiencia> List(){
-    return rExperiencia.findAll();
-}
-    public Optional<Experiencia> getOne(int id){
-        return rExperiencia.findById(id);
-    }
-    
-    public Optional<Experiencia> getByNombreE (String NombreE){
-        return rExperiencia.findByNombreE(NombreE);
-    }
-    public void save(Experiencia expe){
-        rExperiencia.save(expe);
-    }
-    public void delete (int id){
-        rExperiencia.deleteById(id);
-    }
-    public boolean existsById (int id){
-        return rExperiencia.existsById(id);
-    }
-    public boolean existsByNombreE (String nombreE){
-        return rExperiencia.existsByNombreE(nombreE);
-    }
-
-    public List<Experiencia> list() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        return rExperiencia.findAll();
-    }
-
-       
+     @Autowired
+     RExperiencia rExperiencia;
+     
+     public List<Experiencia> list(){
+         return rExperiencia.findAll();
+     }
+     
+     public Optional<Experiencia> getOne(int id){
+         return rExperiencia.findById(id);
+     }
+     
+     public Optional<Experiencia> getByNombreE(String nombreE){
+         return rExperiencia.findByNombreE(nombreE);
+     }
+     
+     public void save(Experiencia expe){
+         rExperiencia.save(expe);
+     }
+     
+     public void delete(int id){
+         rExperiencia.deleteById(id);
+     }
+     
+     public boolean existsById(int id){
+         return rExperiencia.existsById(id);
+     }
+     
+     public boolean existsByNombreE(String nombreE){
+         return rExperiencia.existsByNombreE(nombreE);
+     }
 }
