357dd3c92665ffddeb680d7df7eb87dbb810ad63
lerghin <lerghincardenas@gmail.com>
2023-03-20 09:02:42 -0400

    web

--- src/main/java/com/portfoliolerghin/lc/WebConfig.java
+++ src/main/java/com/portfoliolerghin/lc/WebConfig.java
@@ -9,13 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
 * Clase que habilita CORS
 * @author YOProgramo
-*/
+*///
+
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
  @Override
  public void addCorsMappings(CorsRegistry registry) {
- registry.addMapping("/**");
+ registry.addMapping("/**").allowedOrigins("https://frontenprueba.web.app/")
+                        .allowedMethods("*").allowedHeaders("*");
  }
 }
  
+
+
